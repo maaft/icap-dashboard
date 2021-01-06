d7a681b062ed480ce5a54632d2b2491b7dc0656c
mawe <mawe@maaft.fritz.box>
2021-01-06 21:48:53 +0100

    asfas

--- start_live.sh
+++ start_live.sh
@@ -9,7 +9,8 @@ mkdir -p $ALPHA_PATH_P
 mkdir -p $ALPHA_PATH_W
 
 dgraph alpha -p $PWD/dgraphdb/p -w $PWD/dgraphdb/w &
-sleep 10
 dgraph zero -w $PWD/dgraphdb/zw &
 
-go run ethereum/main.go
+export APP_SECRET="12345"
+sleep 15
+envsubst '${APP_SECRET}' < \"db/dgraph.graphql\" > \"db/dgraph-secret.graphql\";curl -X POST localhost:8080/admin/schema --data-binary '@db/dgraph-secret.graphql'
