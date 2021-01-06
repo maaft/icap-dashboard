a7839297a6c0609a1cbe7af384f92b47ee2469f4
mawe <mawe@maaft.fritz.box>
2021-01-06 22:32:00 +0100

    asfas

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -64,7 +64,14 @@ export default function Overview() {
 
  return  (
    <>
-   <Message color='pink' size="massive">Plz help pay server rent: 0xd023b2179728BB4c232Dd773B0B9e7E4484e6434</Message>
+   <Message color='pink' size="massive">
+     Plz help pay server rent:
+     <br/>
+     BTC: bc1qcdrz98p7w5yhxseyp079ye0tl5jsa6uc2vahwm
+     <br/>
+     ETH: 0xd023b2179728BB4c232Dd773B0B9e7E4484e6434
+     <br/>
+     ADA: addr1qx2xvq69yqnvr6zd3g0j0zhv9sh5cfncyj3ah4eqzhgg4u55djzl65arp65vrfeejaklk7q2hpkxt5pux4hykwna37mqswshql </Message>
  <Segment.Group>
  <Segment style={{ backgroundColor: '#f4f4f4' }}>
    <Header as="h2">Staking Overview</Header>
