bcaa07aef6f02b4641f523407916c38d5f78195f
maaft <asdfg>
2021-05-03 21:24:11 +0200

    increased lp program duration

--- src/components/LPCalculator.tsx
+++ src/components/LPCalculator.tsx
@@ -147,9 +147,9 @@ export default function LPCalculator() {
 
   //@ts-ignore
   const diff = Math.ceil(Math.abs(now - start) / (1000 * 60 * 60 * 24));
-  const daysLeft = 90 - diff > 0 ? 90 - diff : 0;
+  const daysLeft = 180 - diff > 0 ? 180 - diff : 0;
 
-  const lpProgramCapitalLeft = (30000 * daysLeft) / 90;
+  const lpProgramCapitalLeft = (60000 * daysLeft) / 180;
 
   let rewardProgramCapital = 0;
   if (apyScenario === "always-lp-program") {
